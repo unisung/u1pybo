b5f6fa1890e97e7dc96be9f7959e239bd2c6a993
unisung <vctor@naver.com>
2025-04-10 09:31:48 +0900

    Initial commit

---
+++ pybo/urls.py
@@ -0,0 +1,14 @@
+from tkinter.font import names
+
+from django.urls import path
+
+from . import views
+
+app_name = 'pybo' # namespace 추가
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>/',views.detail, name='detail'),
+    path('answer/create/<int:question_id>',views.answer_create,name="answer_create"),
+    path('question/create/',views.question_create,name="question_create")
+]
